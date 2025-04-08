a3cfba2ddffaa62eaa696f048dc026f72c2f2689
leshanpasindu12 <leshanpasindu12@gmail.com>
2025-04-08 05:18:25 +0000

    basic changes

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return(
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contect</a></li>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
