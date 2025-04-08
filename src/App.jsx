bfe02ea2f63da09ffb4b6795387f010598dadd31
leshanpasindu12 <leshanpasindu12@gmail.com>
2025-04-08 11:42:04 +0000

    create button add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
+import Button from './Button/Button.jsx'
 
 function App() {
   return(
     <>
-      <Header/>
-      <Card/>
-      <Footer/>
+      <Button/>
     </>
      
   );
