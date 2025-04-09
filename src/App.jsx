e1ce7352a02271f3d1a57ca9553b79851f07d29e
leshanpasindu12 <leshanpasindu12@gmail.com>
2025-04-09 03:06:12 +0000

    render list

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import Footer from './Footer.jsx'
 import Card from './Card.jsx'
 import Button from './Button/Button.jsx' 
 import Student from './Student/Student.jsx'
-
+import List from './List.jsx'
 
 function App() {
   return(
     <>
-      <Student name="Leshan" age={24} isStudent={true}/>
-      <Student name="Pasindu" age={44} isStudent={false}/>
+      <List/>
     </>
      
   );
